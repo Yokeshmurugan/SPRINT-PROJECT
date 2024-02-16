e2d793a1e6cacf537ed0f4e86012f1b03f7e8926
yokesh <130481125+Yokeshmurugan@users.noreply.github.com>
2024-02-16 20:52:12 +0530

    Delete CertificateRepository.java

--- CertificateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.subha.CertificateService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertificateRepository extends JpaRepository <Certificate,Integer>
-{
-
-}
